0b14faeb34f6540ccfe71674394bef45f1e8c083
Amin Ghorbany <m.a.ghorbany73@gmail.com>
2023-12-24 14:22:05 +0330

    feat : navigation component added.

--- topmovies/src/main/java/com/example/topmovies/ui/MainActivity.kt
+++ topmovies/src/main/java/com/example/topmovies/ui/MainActivity.kt
@@ -2,15 +2,41 @@ package com.example.topmovies.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.topmovies.R
 import com.example.topmovies.databinding.ActivityMainBinding
+import com.example.topmovies.utils.goneWidget
+import com.example.topmovies.utils.showWidget
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.apply {
+            navController = findNavController(R.id.navHost)
+            bottomNav.setupWithNavController(navController)
+            //show bottom navigation
+            navController.addOnDestinationChangedListener { controller, destination, arguments ->
+                if (destination.id == R.id.splashFragment){
+                    goneWidget(bottomNav)
+                }else{
+                    showWidget(bottomNav)
+                }
+            }
+        }
+
+    }
+
+    //handel onBackPress
+    override fun onNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onNavigateUp()
     }
 }
--- topmovies/src/main/java/com/example/topmovies/ui/splash/SplashFragment.kt
+++ topmovies/src/main/java/com/example/topmovies/ui/splash/SplashFragment.kt
@@ -6,11 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.topmovies.R
+import com.example.topmovies.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
 
+    private lateinit var binding: FragmentSplashBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        binding = FragmentSplashBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 }
