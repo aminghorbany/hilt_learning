f1784c7569b82d35bed61fb63e1bdfdd35d581c0
Amin Ghorbany <m.a.ghorbany73@gmail.com>
2023-12-31 15:33:20 +0330

    refactor : DetailFragment refactor.

--- topmovies/src/main/java/com/example/topmovies/viewmodel/DetailViewModel.kt
+++ topmovies/src/main/java/com/example/topmovies/viewmodel/DetailViewModel.kt
@@ -5,14 +5,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.topmovies.db.MoviesEntity
 import com.example.topmovies.models.detail.ResponseDetail
-import com.example.topmovies.models.register.BodyRegister
-import com.example.topmovies.models.register.ResponseRegister
 import com.example.topmovies.repository.DetailRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import okhttp3.internal.wait
 import javax.inject.Inject
 
 @HiltViewModel
